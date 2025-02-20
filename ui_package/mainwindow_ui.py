0a44ca1e4642d440dd57ea882b010e63841aaa41
Yuanzheng (Lucas) Song <song_yuanzheng@hotmail.com>
2025-02-20 14:49:16 -0700

    Refacted fastq analysis tab

--- ui_package/mainwindow_ui.py
+++ ui_package/mainwindow_ui.py
@@ -16,18 +16,23 @@ from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
     QIcon, QImage, QKeySequence, QLinearGradient,
     QPainter, QPalette, QPixmap, QRadialGradient,
     QTransform)
-from PySide6.QtWidgets import (QApplication, QButtonGroup, QCheckBox, QComboBox,
-    QGridLayout, QGroupBox, QHBoxLayout, QLabel,
+from PySide6.QtWidgets import (QAbstractItemView, QApplication, QButtonGroup, QCheckBox,
+    QGroupBox, QHBoxLayout, QHeaderView, QLabel,
     QListWidget, QListWidgetItem, QMainWindow, QMenu,
     QMenuBar, QProgressBar, QPushButton, QRadioButton,
-    QScrollArea, QSizePolicy, QSpacerItem, QStatusBar,
-    QTabWidget, QVBoxLayout, QWidget)
+    QSizePolicy, QSpacerItem, QStatusBar, QTabWidget,
+    QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(901, 629)
+        MainWindow.resize(899, 628)
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
         self.actionSetup = QAction(MainWindow)
         self.actionSetup.setObjectName(u"actionSetup")
         self.actionClear = QAction(MainWindow)
@@ -36,101 +41,123 @@ class Ui_MainWindow(object):
         self.actionQuit.setObjectName(u"actionQuit")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
         self.tabWidget = QTabWidget(self.centralwidget)
         self.tabWidget.setObjectName(u"tabWidget")
-        self.tabWidget.setGeometry(QRect(10, 10, 881, 561))
+        font = QFont()
+        font.setFamilies([u"Arial"])
+        font.setPointSize(13)
+        self.tabWidget.setFont(font)
         self.tab_rawdata = QWidget()
         self.tab_rawdata.setObjectName(u"tab_rawdata")
-        self.groupBox_statistics = QGroupBox(self.tab_rawdata)
-        self.groupBox_statistics.setObjectName(u"groupBox_statistics")
-        self.groupBox_statistics.setGeometry(QRect(10, 10, 851, 511))
-        font = QFont()
-        font.setPointSize(14)
-        font.setBold(True)
-        self.groupBox_statistics.setFont(font)
-        self.verticalLayoutWidget = QWidget(self.groupBox_statistics)
-        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
-        self.verticalLayoutWidget.setGeometry(QRect(10, 30, 621, 51))
-        self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayoutWidget = QWidget(self.tab_rawdata)
+        self.horizontalLayoutWidget.setObjectName(u"horizontalLayoutWidget")
+        self.horizontalLayoutWidget.setGeometry(QRect(10, 0, 861, 531))
+        self.horizontalLayout = QHBoxLayout(self.horizontalLayoutWidget)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.horizontalLayout_2 = QHBoxLayout()
-        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.label_1 = QLabel(self.verticalLayoutWidget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_6 = QVBoxLayout()
+        self.verticalLayout_6.setObjectName(u"verticalLayout_6")
+        self.label_1 = QLabel(self.horizontalLayoutWidget)
         self.label_1.setObjectName(u"label_1")
         self.label_1.setMaximumSize(QSize(16777180, 16777215))
         font1 = QFont()
+        font1.setFamilies([u"Arial"])
         font1.setPointSize(12)
         font1.setBold(True)
         self.label_1.setFont(font1)
         self.label_1.setAlignment(Qt.AlignmentFlag.AlignLeading|Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignVCenter)
 
-        self.horizontalLayout_2.addWidget(self.label_1)
+        self.verticalLayout_6.addWidget(self.label_1)
 
-        self.comboBox = QComboBox(self.verticalLayoutWidget)
-        self.comboBox.setObjectName(u"comboBox")
-        self.comboBox.setFont(font1)
+        self.listWidget_fastq_analysis = QListWidget(self.horizontalLayoutWidget)
+        self.listWidget_fastq_analysis.setObjectName(u"listWidget_fastq_analysis")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Expanding)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.listWidget_fastq_analysis.sizePolicy().hasHeightForWidth())
+        self.listWidget_fastq_analysis.setSizePolicy(sizePolicy1)
+        font2 = QFont()
+        font2.setFamilies([u"Arial"])
+        font2.setPointSize(12)
+        self.listWidget_fastq_analysis.setFont(font2)
+        self.listWidget_fastq_analysis.setAlternatingRowColors(False)
+        self.listWidget_fastq_analysis.setSelectionMode(QAbstractItemView.SelectionMode.MultiSelection)
+        self.listWidget_fastq_analysis.setSelectionRectVisible(False)
 
-        self.horizontalLayout_2.addWidget(self.comboBox)
+        self.verticalLayout_6.addWidget(self.listWidget_fastq_analysis)
+
+        self.verticalLayout_6.setStretch(1, 1)
+
+        self.horizontalLayout.addLayout(self.verticalLayout_6)
+
+        self.verticalLayout_7 = QVBoxLayout()
+        self.verticalLayout_7.setObjectName(u"verticalLayout_7")
+        self.groupBox_fastq_analysis = QGroupBox(self.horizontalLayoutWidget)
+        self.groupBox_fastq_analysis.setObjectName(u"groupBox_fastq_analysis")
+        sizePolicy.setHeightForWidth(self.groupBox_fastq_analysis.sizePolicy().hasHeightForWidth())
+        self.groupBox_fastq_analysis.setSizePolicy(sizePolicy)
+        self.horizontalLayout_3 = QHBoxLayout(self.groupBox_fastq_analysis)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.checkBox_total_bases = QCheckBox(self.groupBox_fastq_analysis)
+        self.checkBox_total_bases.setObjectName(u"checkBox_total_bases")
+        font3 = QFont()
+        font3.setFamilies([u"Arial"])
+        font3.setPointSize(12)
+        font3.setBold(False)
+        self.checkBox_total_bases.setFont(font3)
+        self.checkBox_total_bases.setChecked(True)
+
+        self.horizontalLayout_3.addWidget(self.checkBox_total_bases)
+
+        self.checkBox_total_reads = QCheckBox(self.groupBox_fastq_analysis)
+        self.checkBox_total_reads.setObjectName(u"checkBox_total_reads")
+        self.checkBox_total_reads.setFont(font3)
+        self.checkBox_total_reads.setChecked(True)
+
+        self.horizontalLayout_3.addWidget(self.checkBox_total_reads)
 
         self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
 
-        self.horizontalLayout_2.addItem(self.horizontalSpacer)
+        self.horizontalLayout_3.addItem(self.horizontalSpacer)
 
-        self.checkBox_reads = QCheckBox(self.verticalLayoutWidget)
-        self.checkBox_reads.setObjectName(u"checkBox_reads")
-        self.checkBox_reads.setFont(font1)
+        self.horizontalLayout_3.setStretch(2, 1)
 
-        self.horizontalLayout_2.addWidget(self.checkBox_reads)
+        self.verticalLayout_7.addWidget(self.groupBox_fastq_analysis)
 
-        self.checkBox_bases = QCheckBox(self.verticalLayoutWidget)
-        self.checkBox_bases.setObjectName(u"checkBox_bases")
-        self.checkBox_bases.setFont(font1)
-
-        self.horizontalLayout_2.addWidget(self.checkBox_bases)
-
-
-        self.horizontalLayout.addLayout(self.horizontalLayout_2)
-
-
-        self.verticalLayout.addLayout(self.horizontalLayout)
-
-        self.gridLayoutWidget = QWidget(self.groupBox_statistics)
-        self.gridLayoutWidget.setObjectName(u"gridLayoutWidget")
-        self.gridLayoutWidget.setGeometry(QRect(10, 80, 831, 371))
-        self.gridLayout_2 = QGridLayout(self.gridLayoutWidget)
-        self.gridLayout_2.setObjectName(u"gridLayout_2")
-        self.gridLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.scrollArea = QScrollArea(self.gridLayoutWidget)
-        self.scrollArea.setObjectName(u"scrollArea")
-        self.scrollArea.setWidgetResizable(True)
-        self.scrollAreaWidgetContents = QWidget()
-        self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 734, 317))
-        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
-
-        self.gridLayout_2.addWidget(self.scrollArea, 2, 1, 1, 1)
-
-        self.progressBar_rawdata_analysis = QProgressBar(self.gridLayoutWidget)
-        self.progressBar_rawdata_analysis.setObjectName(u"progressBar_rawdata_analysis")
-        self.progressBar_rawdata_analysis.setValue(24)
-
-        self.gridLayout_2.addWidget(self.progressBar_rawdata_analysis, 1, 1, 1, 1)
-
-        self.verticalLayout_2 = QVBoxLayout()
-        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-
-        self.gridLayout_2.addLayout(self.verticalLayout_2, 0, 0, 1, 1)
-
-        self.button_analysis_start = QPushButton(self.gridLayoutWidget)
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.button_analysis_start = QPushButton(self.horizontalLayoutWidget)
         self.button_analysis_start.setObjectName(u"button_analysis_start")
         self.button_analysis_start.setEnabled(True)
         self.button_analysis_start.setFont(font1)
 
-        self.gridLayout_2.addWidget(self.button_analysis_start, 1, 0, 1, 1)
+        self.horizontalLayout_2.addWidget(self.button_analysis_start)
 
+        self.progressBar_fastq_analysis = QProgressBar(self.horizontalLayoutWidget)
+        self.progressBar_fastq_analysis.setObjectName(u"progressBar_fastq_analysis")
+        self.progressBar_fastq_analysis.setValue(1)
+
+        self.horizontalLayout_2.addWidget(self.progressBar_fastq_analysis)
+
+        self.horizontalLayout_2.setStretch(1, 1)
+
+        self.verticalLayout_7.addLayout(self.horizontalLayout_2)
+
+        self.tableWidget_fastq_analysis = QTableWidget(self.horizontalLayoutWidget)
+        self.tableWidget_fastq_analysis.setObjectName(u"tableWidget_fastq_analysis")
+        self.tableWidget_fastq_analysis.setFont(font2)
+        self.tableWidget_fastq_analysis.setAlternatingRowColors(True)
+
+        self.verticalLayout_7.addWidget(self.tableWidget_fastq_analysis)
+
+        self.verticalLayout_7.setStretch(2, 1)
+
+        self.horizontalLayout.addLayout(self.verticalLayout_7)
+
+        self.horizontalLayout.setStretch(0, 1)
+        self.horizontalLayout.setStretch(1, 4)
         self.tabWidget.addTab(self.tab_rawdata, "")
         self.tab_fastqcreport = QWidget()
         self.tab_fastqcreport.setObjectName(u"tab_fastqcreport")
@@ -148,7 +175,7 @@ class Ui_MainWindow(object):
 
         self.progressBar_fastqc = QProgressBar(self.verticalLayoutWidget_2)
         self.progressBar_fastqc.setObjectName(u"progressBar_fastqc")
-        self.progressBar_fastqc.setValue(24)
+        self.progressBar_fastqc.setValue(1)
 
         self.verticalLayout_3.addWidget(self.progressBar_fastqc)
 
@@ -158,8 +185,6 @@ class Ui_MainWindow(object):
         self.fastqcRadioButtonGroup.addButton(self.radioButton_base_seq_quality)
         self.radioButton_base_seq_quality.setObjectName(u"radioButton_base_seq_quality")
         self.radioButton_base_seq_quality.setEnabled(False)
-        font2 = QFont()
-        font2.setPointSize(12)
         self.radioButton_base_seq_quality.setFont(font2)
 
         self.verticalLayout_3.addWidget(self.radioButton_base_seq_quality)
@@ -174,6 +199,7 @@ class Ui_MainWindow(object):
 
         self.listWidget_fastqcreport = QListWidget(self.verticalLayoutWidget_2)
         self.listWidget_fastqcreport.setObjectName(u"listWidget_fastqcreport")
+        self.listWidget_fastqcreport.setFont(font2)
 
         self.verticalLayout_3.addWidget(self.listWidget_fastqcreport)
 
@@ -185,6 +211,8 @@ class Ui_MainWindow(object):
         self.verticalLayout_4.setContentsMargins(0, 0, 0, 0)
         self.label_image = QLabel(self.verticalLayoutWidget_3)
         self.label_image.setObjectName(u"label_image")
+        sizePolicy.setHeightForWidth(self.label_image.sizePolicy().hasHeightForWidth())
+        self.label_image.setSizePolicy(sizePolicy)
 
         self.verticalLayout_4.addWidget(self.label_image)
 
@@ -192,10 +220,13 @@ class Ui_MainWindow(object):
         self.tab_3 = QWidget()
         self.tab_3.setObjectName(u"tab_3")
         self.tabWidget.addTab(self.tab_3, "")
+
+        self.verticalLayout.addWidget(self.tabWidget)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 901, 24))
+        self.menubar.setGeometry(QRect(0, 0, 899, 22))
         self.menu = QMenu(self.menubar)
         self.menu.setObjectName(u"menu")
         self.menu_2 = QMenu(self.menubar)
@@ -213,7 +244,7 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
 
-        self.tabWidget.setCurrentIndex(1)
+        self.tabWidget.setCurrentIndex(0)
 
 
         QMetaObject.connectSlotsByName(MainWindow)
@@ -224,10 +255,10 @@ class Ui_MainWindow(object):
         self.actionSetup.setText(QCoreApplication.translate("MainWindow", u"\u914d\u7f6e", None))
         self.actionClear.setText(QCoreApplication.translate("MainWindow", u"\u91cd\u7f6e", None))
         self.actionQuit.setText(QCoreApplication.translate("MainWindow", u"\u9000\u51fa", None))
-        self.groupBox_statistics.setTitle(QCoreApplication.translate("MainWindow", u"\u6570\u636e\u91cf\u7edf\u8ba1", None))
         self.label_1.setText(QCoreApplication.translate("MainWindow", u"\u9009\u62e9\u539f\u59cb\u6570\u636e\uff1a", None))
-        self.checkBox_reads.setText(QCoreApplication.translate("MainWindow", u"Total Reads", None))
-        self.checkBox_bases.setText(QCoreApplication.translate("MainWindow", u"Total Bases", None))
+        self.groupBox_fastq_analysis.setTitle(QCoreApplication.translate("MainWindow", u"\u9009\u62e9\u7edf\u8ba1\u9879", None))
+        self.checkBox_total_bases.setText(QCoreApplication.translate("MainWindow", u"Total Bases", None))
+        self.checkBox_total_reads.setText(QCoreApplication.translate("MainWindow", u"Total Reads", None))
         self.button_analysis_start.setText(QCoreApplication.translate("MainWindow", u"\u5f00\u59cb\u5206\u6790", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_rawdata), QCoreApplication.translate("MainWindow", u"\u6d4b\u5e8f\u6570\u636e", None))
         self.pushButton_fastQC_Report.setText(QCoreApplication.translate("MainWindow", u"\u751f\u6210FastQC Report", None))
