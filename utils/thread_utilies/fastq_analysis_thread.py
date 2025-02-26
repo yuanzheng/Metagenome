625c23023d8a178bc53112cf7b73447ce58d68e1
Yuanzheng (Lucas) Song <song_yuanzheng@hotmail.com>
2025-02-26 16:55:59 -0700

    Implemented fastq file analysis processor

--- utils/thread_utilies/fastq_analysis_thread.py
+++ utils/thread_utilies/fastq_analysis_thread.py
@@ -4,6 +4,7 @@ import re
 import platform
 import subprocess
 import logging
+import config.config as config
 from PySide6.QtCore import QRunnable, Signal, QObject
 
 # ==============================
@@ -19,21 +20,18 @@ class AnalysisSignals(QObject):
 # 可执行分析任务线程
 # ==============================
 class AnalysisTask(QRunnable):
-    """"""
-    analysis_result = Signal(str, int, int)  # 文件名, reads, bases
-    analysis_failed = Signal(str, str)       # 文件名, 错误信息
-
-    def __init__(self, file_path, program_path):
+    
+    def __init__(self, file_id, file_path, program_path):
         super().__init__()
         self.logger = logging.getLogger(self.__class__.__name__)
+        self.file_id = file_id  # MT37
         self.file_path = file_path  # (R1路径, R2路径)
         self.program_path = program_path
         self.signals = AnalysisSignals()
         self.setAutoDelete(True)   # 任务完成后自动清理
 
     def run(self):
-        pair_id = self._get_pair_id()
-        self.signals.started.emit(pair_id)
+        self.signals.started.emit(self.file_id)
 
         try:
             self.logger.info("Start fastQ analysis thread!")
@@ -53,23 +51,19 @@ class AnalysisTask(QRunnable):
             
             # 解析输出
             stats = self._parse_output(result.stdout)
-            self.signals.result_ready.emit(pair_id, stats)
+            self.signals.result_ready.emit(self.file_id, stats)
         except subprocess.CalledProcessError as e:
             error_msg = f"Exit code {e.returncode}: {e.stderr.strip()}"
-            self.signals.error_occurred.emit(pair_id, error_msg)
+            self.signals.error_occurred.emit(self.file_id, error_msg)
         except Exception as e:
-            self.signals.error_occurred.emit(pair_id, str(e))
-
-
-    def _get_pair_id(self):
-        """从文件路径提取样本标识（如 MT37）"""
-        base_name = os.path.basename(self.file_path[0])
-        return base_name.split("_")[0]  
+            self.signals.error_occurred.emit(self.file_id, str(e))
 
     def _build_command(self):
         """构建跨平台命令"""
         cmd = [self.program_path]
-
+        cmd.extend(self.file_path)
+        self.logger.debug("Build command: %s", cmd)
+        '''
         # Windows 路径处理
         if platform.system() == "Windows":
             for each_file_path in self.file_path:
@@ -78,6 +72,7 @@ class AnalysisTask(QRunnable):
                 cmd.append(fastq_file_path)
         else:
             cmd.extend(self.file_path)
+        '''
         return cmd
 
     def _parse_output(self, output):
@@ -90,6 +85,6 @@ class AnalysisTask(QRunnable):
             raise ValueError("输出格式不符合预期")
             
         return {
-            "reads": int(reads_match.group(1)),
-            "bases": int(bases_match.group(1))
+            config.FASTQ_TOTAL_READS: int(reads_match.group(1)),
+            config.FASTQ_TOTAL_BASES: int(bases_match.group(1))
         }
